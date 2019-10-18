f91739120b4eab463d81fcf5fc1ab7d67ee8cdf2
Jatsby <workaholic_yzj@163.com>
2019-10-18 21:13:49 +0800

    add index and bootstrap

---
+++ src/main/java/life/coder/community/community/controller/IndexController.java
@@ -0,0 +1,12 @@
+package life.coder.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
